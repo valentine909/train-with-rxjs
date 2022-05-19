40f092ed3cdda96da0c53561fd027fe01dddaac9
valentine909 <valentine.nazarov@gmail.com>
2022-05-19 14:29:48 +0300

    feat: brewery api test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {MainPageComponent} from "./pages/main-page/main-page.component";
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainPageComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  debounceTime,
+  filter,
+  fromEvent,
+  map,
+  Observable,
+  switchMap,
+} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements AfterViewInit {
+  @ViewChild('container') container!: ElementRef<HTMLDivElement>;
+  @ViewChild('search') search!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  url: string = 'https://api.openbrewerydb.org/breweries/search?';
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit(): void {
+    const response$ = fromEvent(this.search.nativeElement, 'keyup').pipe(
+      debounceTime(1000),
+      map(() => this.search.nativeElement.value),
+      filter((key: string) => key.length >= 3),
+      map((key: string) => {
+        return new HttpParams().set('query', key);
+      }),
+      switchMap<HttpParams, Observable<IBrew[]>>((params) =>
+        this.http.get<IBrew[]>(this.url, { params: params })
+      )
+    );
+    response$.subscribe(
+      (data: IBrew[]) =>
+        (this.container.nativeElement.innerHTML = data
+          .map((brew) => brew.name)
+          .join('<br>'))
+    );
   }
+}
 
+interface IBrew {
+  name: string;
 }
