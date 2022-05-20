17a12ac0750b522974fc3b59c33a9a081463820b
Valentine <valentine.nazarov@gmail.com>
2022-05-20 11:58:36 +0300

    feat: add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SecondPageComponent } from './pages/second-page/second-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'first', component: MainPageComponent },
+  {
+    path: 'second',
+    component: SecondPageComponent,
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, MainPageComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, MainPageComponent, SecondPageComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
